91749898541dfa6bd25c48e6b3ffe033f4fe00e4
Matilde Morrone <contact@morrone.dev>
2022-07-30 09:26:15 +0200

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+#![feature(unchecked_math)]
+
+use core::{
+    ffi::{c_char, c_int, CStr},
+    ptr::null,
+};
+
+pub struct Args {
+    next: *const *const c_char,
+    end: *const *const c_char,
+}
+
+impl Args {
+    pub const fn empty() -> Self {
+        Self {
+            next: null(),
+            end: null(),
+        }
+    }
+
+    /// # Safety
+    /// TODO
+    pub const unsafe fn new(argc: c_int, argv: *const *const c_char) -> Self {
+        Self {
+            next: argv,
+            end: argv.offset(argc as isize),
+        }
+    }
+}
+
+impl Iterator for Args {
+    type Item = &'static CStr;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next == self.end {
+            None
+        } else {
+            let c_str = unsafe { CStr::from_ptr(*self.next) };
+            self.next = unsafe { self.next.offset(1) };
+            Some(c_str)
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.len();
+        (len, Some(len))
+    }
+}
+
+impl ExactSizeIterator for Args {
+    fn len(&self) -> usize {
+        // self.end as usize - self.next as usize
+        // FIXME: Requires stabilization of unchecked_math. see rust-lang/rust#85122
+        unsafe { (self.end as usize).unchecked_sub(self.next as usize) }
+    }
+}
+
+static mut ARGC: c_int = 0;
+static mut ARGV: *const *const c_char = null();
+
+#[cfg(all(target_os = "linux", target_env = "gnu"))]
+#[used]
+#[link_section = ".init_array"]
+static ARGV_INIT_ARRAY: extern "C" fn(c_int, *const *const c_char, *const *const c_char) = {
+    extern "C" fn init_wrapper(
+        argc: c_int,
+        argv: *const *const c_char,
+        _envp: *const *const c_char,
+    ) {
+        unsafe {
+            ARGC = argc;
+            ARGV = argv;
+        }
+    }
+    init_wrapper
+};
+
+#[must_use]
+pub fn args() -> Args {
+    unsafe {
+        if ARGV.is_null() {
+            Args::empty()
+        } else {
+            Args::new(ARGC, ARGV)
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+#![windows_subsystem = "console"]
+
+use core::{
+    arch::asm,
+    ptr::{null, null_mut},
+};
+
+#[cfg(all(target_os = "windows", target_env = "msvc"))]
+use windows::{
+    core::PWSTR,
+    Win32::System::{LibraryLoader::GetModuleFileNameW, Threading::PEB},
+};
+
+#[cfg(all(target_os = "windows", target_env = "msvc"))]
+static mut ARGS: PWSTR = PWSTR(null_mut());
+
+#[cfg(all(target_os = "windows", target_env = "msvc"))]
+#[used]
+#[link_section = ".CRT$XCU"]
+static ARGS_INIT: extern "C" fn() = {
+    extern "C" fn init_args() {
+        let peb: *mut PEB;
+        unsafe {
+            asm!(
+              "mov {}, gs:[0x60]",
+              out(reg) peb,
+              options(pure, nomem, nostack)
+            );
+        }
+
+        unsafe { ARGS = (*(*peb).ProcessParameters).CommandLine.Buffer }
+    }
+    init_args
+};
+
+fn main() {
+    for argument in std::env::args_os() {
+        println!("{argument:?}");
+    }
+
+    for arg in sap::args() {
+        dbg!(arg);
+    }
+
+    #[cfg(all(target_os = "windows", target_env = "msvc"))]
+    unsafe {
+        if !ARGS.is_null() {
+            dbg!(ARGS.to_string().unwrap());
+        }
+
+        let mut buf = [0u16; 512];
+        GetModuleFileNameW(None, &mut buf);
+
+        dbg!(PWSTR::from_raw(buf.as_mut_ptr()).to_string().unwrap());
+    }
+}
