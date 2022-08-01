55f0c61b1ed77c825d0e30e98e432342ebf20f85
Matilde Morrone <contact@morrone.dev>
2022-08-01 05:04:32 +0200

    Make no_std example compile on stable

--- examples/no_std.rs
+++ examples/no_std.rs
@@ -1,7 +1,8 @@
-#![feature(lang_items)] // We neeed to enable this to define the `eh_personality`
-#![no_std]
-#![no_main]
+#![cfg_attr(not(feature = "std"), feature(lang_items))] // We neeed to enable this to define the `eh_personality`
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), no_main)]
 
+#[cfg(not(feature = "std"))]
 #[no_mangle]
 // Notice how we don't need to pass argc or argv.
 
@@ -17,10 +18,17 @@ pub extern "C" fn main() -> isize {
 
 // Below we define the required language items
 
+#[cfg(not(feature = "std"))]
 #[panic_handler]
 fn panic(_panic_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(not(feature = "std"))]
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
+
+#[cfg(feature = "std")]
+fn main() {
+    panic!("Tried to run this example on stable and/or without disabling std")
+}
