16ba38b2118fb5ed02bde2a73213fa2ce6aebf2a
Matilde Morrone <contact@morrone.dev>
2023-04-12 04:11:33 +0200

    Manually implement Debug for Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,48 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(not(feature = "std"), feature(unchecked_math))]
-// #![deny(unsafe_op_in_unsafe_fn)]
+use std::{ffi::OsString, fmt::Debug, os::unix::prelude::OsStrExt};
 
-#[cfg(not(feature = "std"))]
-use core::ffi::CStr;
-
-#[cfg(feature = "std")]
-use std::ffi::CStr;
-
-use static_args::StaticArgs;
-
-pub struct Args {
-    inner: StaticArgs,
+pub enum Error {
+    Unexpected,
+    Missing,
 }
 
-#[derive(Debug)]
-pub struct Arg<'a> {
-    inner: ArgInner<'a>,
-}
-
-#[derive(Debug)]
-enum ArgInner<'a> {
-    Unicode(&'a str),
-    Raw(&'a CStr),
-}
-
-impl<'a> ArgInner<'a> {
-    pub fn new(raw: &'a CStr) -> Self {
-        if let Ok(unicode) = raw.to_str() {
-            Self::Unicode(unicode)
-        } else {
-            Self::Raw(raw)
-        }
-    }
-
-    pub fn as_bytes(&self) -> &[u8] {
+impl Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ArgInner::Unicode(unicode) => unicode.as_bytes(),
-            ArgInner::Raw(raw) => raw.to_bytes(),
+            Self::Unexpected => write!(f, "Unexpected argument"),
+            Self::Missing => write!(f, "Missing argument"),
         }
     }
+}
 
-    pub fn is_empty(&self) -> bool {
-        self.as_bytes().is_empty()
-    }
-
-    pub fn is_stdio(&self) -> bool {
-        self.as_bytes() == b"-"
-    }
-
-    pub fn is_escape(&self) -> bool {
-        self.as_bytes() == b"--"
-    }
-
-    pub fn is_long(&self) -> bool {
-        self.as_bytes().starts_with(b"--") && !self.is_escape()
-    }
-
-    pub fn is_short(&self) -> bool {
-        self.as_bytes().starts_with(b"-") && !self.is_stdio() && !self.is_long()
-    }
+#[derive(Debug)]
+pub enum Arg<'a> {
+    Short(&'a [u8]),
+    Long(&'a [u8]),
+    Value(OsString),
 }
 
 impl<'a> Arg<'a> {
-    pub fn new(inner: &'a CStr) -> Self {
-        Self {
-            inner: ArgInner::new(inner),
+    pub fn expect_value(self) -> Option<OsString> {
+        if let Self::Value(value) = self {
+            Some(value)
+        } else {
+            None
         }
     }
-
-    pub fn is_empty(&self) -> bool {
-        self.inner.is_empty()
-    }
-
-    pub fn is_stdio(&self) -> bool {
-        self.inner.is_stdio()
-    }
-
-    pub fn is_escape(&self) -> bool {
-        self.inner.is_escape()
-    }
-
-    pub fn is_long(&self) -> bool {
-        self.inner.is_long()
-    }
-
-    pub fn is_short(&self) -> bool {
-        self.inner.is_short()
-    }
 }
 
-impl Args {
-    pub const fn new(inner: StaticArgs) -> Self {
-        Self { inner }
-    }
+pub trait ParseArg {
+    fn parse(&self) -> Arg;
 }
 
-impl Iterator for Args {
-    type Item = Arg<'static>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next().map(Arg::new)
+impl ParseArg for OsString {
+    fn parse(&self) -> Arg {
+        if let Some(arg) = self.as_bytes().strip_prefix(b"--") {
+            Arg::Long(arg)
+        } else if let Some(arg) = self.as_bytes().strip_prefix(b"-") {
+            Arg::Short(arg)
+        } else {
+            Arg::Value(self.to_os_string())
+        }
     }
 }
