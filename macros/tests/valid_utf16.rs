b8c785dbef18232e6a2b01cbe0dfe20e1156f899
Federico Maria Morrone <contact@morrone.dev>
2023-04-23 00:43:01 +0200

    Removed old test

--- macros/tests/valid_utf16.rs
+++
@@ -1,9 +0,0 @@
-use sap_macros::utf16_string;
-
-#[test]
-fn is_valid_utf16() {
-    let known_good: Vec<u16> = "This is an example string".encode_utf16().collect();
-    let constructed = utf16_string!("This is an example string");
-
-    assert_eq!(&known_good, &constructed);
-}
