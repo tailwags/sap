97981c0d26b940ce61fc462ed8e2d46930585907
Matilde Morrone <contact@morrone.dev>
2025-03-13 18:30:39 +0100

    Remove old implementation

--- examples/derive.rs
+++ examples/derive.rs
@@ -1,21 +1,3 @@
-use anyhow::Result;
-use sap::Parser;
-use std::ffi::OsString;
-
-#[derive(Debug, Parser)]
-#[allow(unused)]
-struct Args {
-    normal: OsString,
-    utf8: String,
-    flag: bool,
-    optional: Option<OsString>,
-    multiple: Vec<OsString>,
-}
-
-fn main() -> Result<()> {
-    let args = Args::try_parse(std::env::args_os().skip(1))?;
-
-    dbg!(args);
-
-    Ok(())
+fn main() {
+    
 }
